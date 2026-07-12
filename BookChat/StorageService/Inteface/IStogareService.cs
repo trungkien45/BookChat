9bb4abf453a2d14dc1e03366a87ba254de1984d7
trungkien45 <ntkien45@gmail.com>
2026-07-13 00:17:26 +0700

    Refactor code

---
+++ BookChat/StorageService/Inteface/IStogareService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookChat.StorageService.Inteface
+{
+    public interface IStogareService
+    {
+        public Task<List<StorageItem>> GetFilesAndFolders(StorageItem storageItem);
+        public Task<bool> Delete(StorageItem storageItem);
+        public Task<bool> CreateFolder(StorageItem storageItem, string folderName);
+        public Task<bool> Move(StorageItem source, StorageItem destination);
+        public Task<bool> Rename(StorageItem storageItem, string newName);
+    }
+}
---
+++ BookChat/StorageService/StorageItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookChat.StorageService
+{
+    public class StorageItem
+    {
+        /// <summary>
+        /// Android: the tree Uri of the selected storage.
+        /// in windows, the id is the fullpath of the file or folder
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// in android, the document id is the document id of the file or folder
+        /// in windows, the document id is the name of the file or folder
+        /// </summary>
+        public string DocumentId { get; set; } = string.Empty;
+        /// <summary>
+        /// in android, the parent document id is the id of the parent folder
+        /// in windows, the parent document id is the fullpath of the parent folder
+        /// </summary>
+        public string ParentDocumentId { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
+        public bool IsDirectory { get; set; }
+    }
+}
